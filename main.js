49710cb8f2db9e13ea2ec8f9654bfddccd04c411
Rami <114432079+Ramiii0@users.noreply.github.com>
2024-07-28 13:38:27 +0300

    Update main.js

--- main.js
+++ main.js
@@ -11,31 +11,31 @@ function showPosition(position) {
     const longitude = position.coords.longitude;
     console.log(latitude+","+longitude)
     var data1 = latitude+","+longitude
-    var data = JSON.stringify({
-        "collection": "api",
-        "database": "natours",
-        "dataSource": "Cluster0",
-        "document": {
-          "Coordinationa": `${data1}`
-        }
+  const data = JSON.stringify({
+    "collection": "api",
+    "database": "natours",
+    "dataSource": "Cluster0",
+    "document": {
+      "Coordination":`${data1}`
+    }
+  });
+  
+  const config = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Headers': '*',
+      'api-key': '5dwXV6WAsIqxXHkrqqrJxRFTepsuEdAcfTStmHEGt9qfL6NyeJfyO5vuU4tkQawe',
+    },
+    body: data
+  };
+  
+  fetch('https://eu-central-1.aws.data.mongodb-api.com/app/data-bpddjre/endpoint/data/v1/action/insertOne', config)
+    .then(response => response.json())
+    .then(data => {
+      console.log(JSON.stringify(data));
+    })
+    .catch(error => {
+      console.error('Error:', error);
     });
-    
-    var config = {
-        method: 'post',
-        url: 'https://eu-central-1.aws.data.mongodb-api.com/app/data-bpddjre/endpoint/data/v1/action/insertOne',
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Request-Headers': '*',
-          'api-key': '5dwXV6WAsIqxXHkrqqrJxRFTepsuEdAcfTStmHEGt9qfL6NyeJfyO5vuU4tkQawe',
-        },
-        data: data
-    };
-    
-    axios(config)
-        .then(function (response) {
-            console.log(JSON.stringify(response.data));
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
 }
