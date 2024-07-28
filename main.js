8e4680d839e57cdfb42d9a866886bc7d72b062ff
Rami <114432079+Ramiii0@users.noreply.github.com>
2024-07-28 13:27:44 +0300

    Update main.js

--- main.js
+++ main.js
@@ -9,4 +9,33 @@ function getLocation() {
 function showPosition(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
-    console.log(latitude+","+longitude)}
+    console.log(latitude+","+longitude)
+    var data1 = latitude+","+longitude
+    var data = JSON.stringify({
+        "collection": "api",
+        "database": "natours",
+        "dataSource": "Cluster0",
+        "document": {
+          "Coordinationa": `${data1}`
+        }
+    });
+    
+    var config = {
+        method: 'post',
+        url: 'https://eu-central-1.aws.data.mongodb-api.com/app/data-bpddjre/endpoint/data/v1/action/insertOne',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Request-Headers': '*',
+          'api-key': '5dwXV6WAsIqxXHkrqqrJxRFTepsuEdAcfTStmHEGt9qfL6NyeJfyO5vuU4tkQawe',
+        },
+        data: data
+    };
+    
+    axios(config)
+        .then(function (response) {
+            console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
